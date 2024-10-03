a1b10e2be2b7e5aed5708e516fbf7df73dbdc445
Moemen Salem <moemen@example.com>
2024-10-03 06:46:10 +0300

    add add note cubit to add note bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_module_3/cubits/cubit/add_notes_cubit_cubit.dart';
 import 'package:flutter_module_3/models/note_model.g.dart';
 import 'package:flutter_module_3/views/notes_view.dart';
 import 'package:flutter_module_3/widgets/constants.dart';
@@ -16,10 +18,15 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowMaterialGrid: false,
-      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-      home: const NotesView(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AddNotesCubitCubit()),
+      ],
+      child: MaterialApp(
+        debugShowMaterialGrid: false,
+        theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+        home: const NotesView(),
+      ),
     );
   }
 }
--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_module_3/cubits/cubit/add_notes_cubit_cubit.dart';
+import 'package:flutter_module_3/widgets/add_note_form.dart';
+// ignore: unused_import
 import 'package:flutter_module_3/widgets/custom_button.dart';
+// ignore: unused_import
 import 'package:flutter_module_3/widgets/custom_text_field.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
@@ -10,66 +16,21 @@ class AddNoteBottomSheet extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: AddNoteForm(),
-      ),
-    );
-  }
-}
-
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({
-    super.key,
-  });
-
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
-
-class _AddNoteFormState extends State<AddNoteForm> {
-  final GlobalKey<FormState> formKey = GlobalKey();
-
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-  String? title, subtitle;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      autovalidateMode: autovalidateMode,
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextField(
-            onSaved: (value) {
-              title = value;
-            },
-            hint: 'Titel',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            onSaved: (value) {
-              subtitle = value;
-            },
-            hint: 'content',
-            maxLines: 5,
-          ),
-          SizedBox(
-            height: 90,
-          ),
-          CustomButton(
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
-            },
-          )
-        ],
+        child: BlocConsumer<AddNotesCubitCubit, AddNotesCubitState>(
+          listener: (context, state) {
+            if (state is AddNotesCubitFailure){
+              print('failied ${state.errMessage}');
+            }
+            if(state is AddNotesCubitFailure){
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+                inAsyncCall: state is AddNotesCubitLoading ? true : false,
+                child: AddNoteForm());
+          },
+        ),
       ),
     );
   }
